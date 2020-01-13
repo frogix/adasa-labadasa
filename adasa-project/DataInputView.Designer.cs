1a2661b6052f3f57ffb3d784feb5ab1dae3a5df6
Arseny Neutroev <neustroev.arseny@gmail.com>
2020-01-13 22:33:24 +0500

    added data input view

---
+++ adasa-project/DataInputView.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace adasa_project
+{
+    partial class DataInputView
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LeftComboBox = new System.Windows.Forms.ComboBox();
+            this.RightComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addConnectionButton = new System.Windows.Forms.Button();
+            this.dependenciesList = new System.Windows.Forms.ListBox();
+            this.ComputeButton = new System.Windows.Forms.Button();
+            this.DeleteConnectionButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(287, 128);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 50);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество вершин";
+            // 
+            // LeftComboBox
+            // 
+            this.LeftComboBox.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LeftComboBox.FormattingEnabled = true;
+            this.LeftComboBox.Location = new System.Drawing.Point(115, 270);
+            this.LeftComboBox.Name = "LeftComboBox";
+            this.LeftComboBox.Size = new System.Drawing.Size(121, 50);
+            this.LeftComboBox.TabIndex = 2;
+            // 
+            // RightComboBox
+            // 
+            this.RightComboBox.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RightComboBox.FormattingEnabled = true;
+            this.RightComboBox.Location = new System.Drawing.Point(440, 270);
+            this.RightComboBox.Name = "RightComboBox";
+            this.RightComboBox.Size = new System.Drawing.Size(121, 50);
+            this.RightComboBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(316, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 42);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "=>";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(206, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 42);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введите связь";
+            // 
+            // addConnectionButton
+            // 
+            this.addConnectionButton.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addConnectionButton.Location = new System.Drawing.Point(223, 360);
+            this.addConnectionButton.Name = "addConnectionButton";
+            this.addConnectionButton.Size = new System.Drawing.Size(237, 59);
+            this.addConnectionButton.TabIndex = 6;
+            this.addConnectionButton.Text = "Добавить!";
+            this.addConnectionButton.UseVisualStyleBackColor = true;
+            this.addConnectionButton.Click += new System.EventHandler(this.addConnectionButton_Click);
+            // 
+            // dependenciesList
+            // 
+            this.dependenciesList.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dependenciesList.FormattingEnabled = true;
+            this.dependenciesList.ItemHeight = 42;
+            this.dependenciesList.Location = new System.Drawing.Point(94, 454);
+            this.dependenciesList.Name = "dependenciesList";
+            this.dependenciesList.Size = new System.Drawing.Size(366, 256);
+            this.dependenciesList.TabIndex = 7;
+            // 
+            // ComputeButton
+            // 
+            this.ComputeButton.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComputeButton.Location = new System.Drawing.Point(260, 750);
+            this.ComputeButton.Name = "ComputeButton";
+            this.ComputeButton.Size = new System.Drawing.Size(287, 72);
+            this.ComputeButton.TabIndex = 8;
+            this.ComputeButton.Text = "Найти!";
+            this.ComputeButton.UseVisualStyleBackColor = true;
+            this.ComputeButton.Click += new System.EventHandler(this.ComputeButton_Click);
+            // 
+            // DeleteConnectionButton
+            // 
+            this.DeleteConnectionButton.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteConnectionButton.Location = new System.Drawing.Point(486, 518);
+            this.DeleteConnectionButton.Name = "DeleteConnectionButton";
+            this.DeleteConnectionButton.Size = new System.Drawing.Size(237, 113);
+            this.DeleteConnectionButton.TabIndex = 9;
+            this.DeleteConnectionButton.Text = "Удалить связь!";
+            this.DeleteConnectionButton.UseVisualStyleBackColor = true;
+            this.DeleteConnectionButton.Click += new System.EventHandler(this.DeleteConnectionButton_Click);
+            // 
+            // DataInputView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 882);
+            this.Controls.Add(this.DeleteConnectionButton);
+            this.Controls.Add(this.ComputeButton);
+            this.Controls.Add(this.dependenciesList);
+            this.Controls.Add(this.addConnectionButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RightComboBox);
+            this.Controls.Add(this.LeftComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "DataInputView";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox LeftComboBox;
+        private System.Windows.Forms.ComboBox RightComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button addConnectionButton;
+        private System.Windows.Forms.ListBox dependenciesList;
+        private System.Windows.Forms.Button ComputeButton;
+        private System.Windows.Forms.Button DeleteConnectionButton;
+    }
+}
+
---
+++ adasa-project/DataInputView.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adasa_project
+{
+    public partial class DataInputView : Form
+    {
+        List<Tuple<string, string>> dependencies = new List<Tuple<string, string>>();
+        public DataInputView()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int numberOfNodes = (int)numericUpDown1.Value;
+            RightComboBox.Items.Clear();
+            LeftComboBox.Items.Clear();
+            for (int i = 0; i < numberOfNodes; i++)
+            {
+                LeftComboBox.Items.Add("V" + (i + 1).ToString());
+                RightComboBox.Items.Add("U" + (i + 1).ToString());
+            }
+        }
+
+        private void addConnectionButton_Click(object sender, EventArgs e)
+        {
+            if (RightComboBox.SelectedItem == null || LeftComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("One or more jobs were not selected in the checkboxes. Try selecting both!");
+                return;
+            }
+            Tuple<string, string> pairToInput = new Tuple<string, string>(LeftComboBox.SelectedItem.ToString(), RightComboBox.SelectedItem.ToString());
+            if (dependencies.Contains(pairToInput))
+            {
+                MessageBox.Show("Unable to add dependency that is already in the list!");
+                return;
+            }
+            dependencies.Add(new Tuple<string, string>(pairToInput.Item1, pairToInput.Item2));
+            UpdateDependenciesList();
+        }
+
+        private void UpdateDependenciesList()
+        {
+            dependenciesList.Items.Clear();
+            foreach (Tuple<string, string> keyValue in dependencies)
+            {
+                dependenciesList.Items.Add(keyValue.Item1 + " -> " + keyValue.Item2);
+            }
+        }
+
+        private void DeleteConnectionButton_Click(object sender, EventArgs e)
+        {
+            if (dependenciesList.SelectedItem == null)
+            {
+                MessageBox.Show("No dependency selected to delete. Firstly choose one from the list");
+                return;
+            }
+            string stringFromList = dependenciesList.SelectedItem.ToString();
+            Tuple<string, string> valueToDelete = new Tuple<string, string>(stringFromList.Substring(0,2), stringFromList.Substring(6));
+            dependencies.Remove(valueToDelete);
+            UpdateDependenciesList();
+        }
+
+        private void ComputeButton_Click(object sender, EventArgs e)
+        {
+            bool[,] outputDependecies = new bool[(int)numericUpDown1.Value, (int)numericUpDown1.Value];
+            foreach (Tuple<string, string> dependency in dependencies)
+            {
+                int leftNumber = int.Parse(dependency.Item1[1].ToString());
+                int rightNumber = int.Parse(dependency.Item2[1].ToString());
+                outputDependecies[leftNumber - 1, rightNumber - 1] = true;
+            }
+
+            for (int i = 0; i < outputDependecies.GetLength(0); i++)
+            {
+                for (int j = 0; j < outputDependecies.GetLength(0); j++)
+                {
+                    Console.Write(outputDependecies[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
